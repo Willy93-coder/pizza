761df4db03dd857e3fa85144928ac7a1e4b759d4
Willy93-coder <guillecg13@gmail.com>
2023-01-23 19:29:17 +0100

    Finalización de la clase pizza y comprobación en el main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Pizza pizza1 = new Pizza("margarita", "familiar");
+        System.out.println("Pizza 1: " + pizza1);
+        pizza1.sirve();
+        Pizza pizza2 = new Pizza("funghi", "mediana");
+        System.out.println("Pizza 2: " + pizza2);
+        pizza2.sirve();
+
+        Pizza pizza3 = new Pizza("Dos quesos", "petita");
+        System.out.println("Pizza 3: " + pizza3);
+
+        System.out.println("Pedidas: " + Pizza.getTotalDemanades());
+        System.out.println("Servidas: " + Pizza.getTotalServides());
     }
 }
--- src/Pizza.java
+++ src/Pizza.java
@@ -1,2 +1,69 @@
-package PACKAGE_NAME;public class Pizza {
+/*
+* @author Willy
+* @version 1.0.0
+* */
+
+public class Pizza {
+    // Atributos
+    private String ingrediente;
+    private String tamanio;
+    private String estado;
+    private static int pedidas;
+    private static int servidas;
+
+    // constructores
+    public Pizza() {
+    }
+
+    public Pizza(String ingrediente, String tamanio) {
+        comprobarIngredientes(ingrediente);
+        comprobarTamanio(tamanio);
+        this.estado = "pedida";
+        this.pedidas += 1;
+    }
+
+    // Métodos
+
+    /** Método para contar todas las pizzas servidas
+     * */
+    public void sirve() {
+        this.servidas += 1;
+        System.out.println("Esta pizza ya esta servida");
+    }
+
+    public static int getTotalDemanades() {
+        return pedidas;
+    }
+
+    public static int getTotalServides() {
+        return servidas;
+    }
+
+    // Metodos de comprobacion
+
+    /**Método para comprobar que el tamaño de la pizza es correcto
+     * @param tamanio*/
+    private void comprobarTamanio(String tamanio) {
+        if (tamanio.equals("mitjana") || tamanio.equals("familiar")) {
+             this.tamanio = tamanio;
+        } else {
+            this.tamanio =  "El tamaño de la pizza que ha pedido no lo tenemos";
+        }
+    }
+
+    /**Método para comprobar que los ingredientes de la pizza son correctos
+     * @param ingrediente*/
+    private void comprobarIngredientes(String ingrediente) {
+        switch (ingrediente) {
+            case "margarita", "quatre formatges", "funghi" -> this.ingrediente = ingrediente;
+            default -> this.ingrediente = "No tenemos ese ingrediente";
+        }
+    }
+
+    // To string
+
+    @Override
+    public String toString() {
+        return "Pizza " + ingrediente + " " + tamanio + ", " + estado;
+    }
 }
